c0df29a21f398a046a5428395bb47a2630ebecca
Robert Ohly <ohly0001@algonquinlive.com>
2025-11-09 23:59:30 -0500

    Added persistence skeleton

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Create a .env file with the following fields\
 `ADDRESS='Bot account address goes here'`\
 `APP_PASS='Bot account app password goes here. Whitespace delimiters are important'`\
 `ACCESS_TOKEN='Hugging Face user access token goes here'`\
-`MODEL=meta-llama/Llama-2-7b-chat-hf`
+`MODEL=meta-llama/Llama-2-7b-chat-hf`\
+`TF_ENABLE_ONEDNN_OPTS=0`
---
+++ persistence.py
@@ -0,0 +1,45 @@
+import atexit
+import os
+from dotenv import load_dotenv
+import mysql.connector
+
+load_dotenv()
+
+class db_controller:
+	INSERT_MAIL="INSERT INTO emails (sender, subject, body) VALUES (%s, %s, %s)"
+	SELECT_ONE=""
+
+	def __init__(self):
+		self.mydb=mysql.connector.connect(
+			host="localhost",
+			user=os.getenv('MYSQL_USER'),
+			password=os.getenv('MYSQL_PASS'),
+			database=os.getenv('MYSQL_DB')
+		)
+		self.mycursor=self.mydb.cursor(dictionary=True)
+
+		atexit.register(self._cleanup)
+
+	def create(self):
+		pass
+
+	def read_one(self):
+		pass
+
+	def read_all(self):
+		pass
+
+	def update(self):
+		pass
+
+	def delete(self):
+		pass
+
+def _cleanup(self):
+		if notself.mycursor:
+			self.mycursor.close()
+		if notself.mydb:
+			self.mydb.close()
+
+if __name__=='__main__':
+	db=db_controller()
